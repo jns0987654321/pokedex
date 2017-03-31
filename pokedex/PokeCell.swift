e0c66743736c04d7438e047916f61f4a28be1179
Junior Samaroo <jns0987654321@gmail.com>
2017-03-30 21:25:18 -0400

    Added PokeCell class

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Junior Samaroo on 2017-03-30.
+//  Copyright © 2017 Junior Samaroo. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+    
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
